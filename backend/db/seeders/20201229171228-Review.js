c14c689a746139ab8d13c2ce79ff7e159af5aaab
kristenflorey <64163075+kristenflorey@users.noreply.github.com>
2021-01-29 11:31:59 -0700

    updated review seeder

--- backend/db/seeders/20201229171228-Review.js
+++ backend/db/seeders/20201229171228-Review.js
@@ -2,25 +2,20 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Reviews', [
+      {
+      user_id: 4,
+      bar_id: 85,
+      roll_id: null,
+      rating: 4,
+      review: 'Kamiya is so great omg',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+    return queryInterface.bulkDelete("Reviews", null, {});
+  },
 };
